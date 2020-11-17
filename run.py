52a466180ed4ff8d840870c5c979b70d31937818
Alexei Osinovsky <dqdongshi@163.com>
2020-11-17 15:34:24 +0800

    fix NSGAII info file attributes

--- run.py
+++ run.py
@@ -2,8 +2,12 @@
 
 from src import Controller
 # from src import Analyzer
+from src import Comparator
 
-Controller.run('binary_objective_order.json')
+Controller.run('binary_nsgaii.json')
+# Controller.run('classic_3_binary')
 
-# sheet = Analyzer('xuan_binary_test').make_sheet()
+Comparator('xuan_binary_nsga').compare()
+
+# sheet = Analyzer('xuan_binary_nsga').make_sheet()
 # Analyzer.tabulate('tmp1.csv', sheet)
