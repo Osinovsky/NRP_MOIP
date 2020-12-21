942d27274c56a5d00bea0dcc1d333dd5463dc530
Alexei Osinovsky <dqdongshi@163.com>
2020-12-21 10:49:12 +0800

    min customers for advec

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 # HERE IS ON HOW TO LOAD TASKS AND RUN THEM
 
 from src import Controller
-from src import Analyzer
+# from src import Analyzer
 
-Controller.run('binary_nsgaii_test.json')
+Controller.run('bincst_min_customers_advec.json')
 
 # sheet = Analyzer('classic_nsga').make_sheet()
 # Analyzer.tabulate('tmp1.csv', sheet)
