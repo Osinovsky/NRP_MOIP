74e75429a61a5fff5c3939bb9b68e752be7e1ad4
Alexei Osinovsky <dqdongshi@163.com>
2021-01-13 10:04:24 +0800

    prepare bincst addon

--- run.py
+++ run.py
@@ -3,12 +3,12 @@
 from src import Controller
 from src import Analyzer, Result
 
-Controller.run('binary_moea/realistic_moea.json')
+Controller.run('tmp.json')
 
 # Result.quick_prob('triurgency_exact')
 
-sheet = Analyzer('realistic_binary_moea').make_sheet()
-Analyzer.tabulate('realistic_binary_moea.csv', sheet)
+# sheet = Analyzer('realistic_binary_moea').make_sheet()
+# Analyzer.tabulate('realistic_binary_moea.csv', sheet)
 # analyzer = Analyzer('xuan_binary_')
 # for index in range(1, 6):
 #     project = 'classic_' + str(index)
