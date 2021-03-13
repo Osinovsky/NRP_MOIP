0956a3d95e2ddb1f938574d77a50b0cd7b075578
Alexei Osinovsky <dqdongshi@163.com>
2021-03-13 19:52:48 +0800

    tmp for nc

--- run.py
+++ run.py
@@ -4,19 +4,19 @@ import gc
 from src import Controller
 from src import Analyzer
 
-Controller.run('baan/all.json')
+Controller.run('tmp.json')
 
 gc.collect()
 
-for folder in ['baan_binary_exact', 'baan_bincst_exact']:
-    analyzer = Analyzer(folder)
-    sheet = analyzer.make_better_sheet(methods=['epsilon', 'imprec', 'cwmoip'], indicators=['igd', 'hv', 'evenness'])
-    Analyzer.tabulate(folder + '.csv', sheet)
+# for folder in ['baan_binary_exact', 'baan_bincst_exact']:
+#     analyzer = Analyzer(folder)
+#     sheet = analyzer.make_better_sheet(methods=['epsilon', 'imprec', 'cwmoip'], indicators=['igd', 'hv', 'evenness'])
+#     Analyzer.tabulate(folder + '.csv', sheet)
 
-for folder in ['baan_binary_moea', 'baan_bincst_moea']:
-    analyzer = Analyzer(folder)
-    sheet = analyzer.make_better_sheet(methods=['imprec', 'NSGAII'], indicators=['igd', 'hv', 'evenness'])
-    Analyzer.tabulate(folder + '.csv', sheet)
+# for folder in ['baan_binary_moea', 'baan_bincst_moea']:
+#     analyzer = Analyzer(folder)
+#     sheet = analyzer.make_better_sheet(methods=['imprec', 'NSGAII'], indicators=['igd', 'hv', 'evenness'])
+#     Analyzer.tabulate(folder + '.csv', sheet)
 
 folder = 'baan_triurgency'
 analyzer = Analyzer(folder)
