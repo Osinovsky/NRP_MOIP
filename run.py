cd1b820fd4bfd639bf571e38d4dc57adc8fe4926
Alexei Osinovsky <dqdongshi@163.com>
2021-11-24 09:36:16 +0800

    tidy up

--- run.py
+++ run.py
@@ -2,23 +2,7 @@
 
 import gc
 from src import Controller
-from src import Analyzer
 
-Controller.run('tmp.json')
+Controller.run('template.json')
 
 gc.collect()
-
-# for folder in ['baan_binary_exact', 'baan_bincst_exact']:
-#     analyzer = Analyzer(folder)
-#     sheet = analyzer.make_better_sheet(methods=['epsilon', 'imprec', 'cwmoip'], indicators=['igd', 'hv', 'evenness'])
-#     Analyzer.tabulate(folder + '.csv', sheet)
-
-# for folder in ['baan_binary_moea', 'baan_bincst_moea']:
-#     analyzer = Analyzer(folder)
-#     sheet = analyzer.make_better_sheet(methods=['imprec', 'NSGAII'], indicators=['igd', 'hv', 'evenness'])
-#     Analyzer.tabulate(folder + '.csv', sheet)
-
-folder = 'baan_triurgency'
-analyzer = Analyzer(folder)
-sheet = analyzer.make_better_sheet(methods=['normal', 'NSGAII'], indicators=['igd', 'hv', 'evenness'])
-Analyzer.tabulate(folder + '.csv', sheet)
